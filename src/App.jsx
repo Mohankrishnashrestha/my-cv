3a83325283cb4aaeddf09b74b261367fc22831dc
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-26 21:41:31 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import Header from "./Header"
-
+import Header from "./Header";
+import "./App.css"
 function App() {
- const information={
-  name : "Mohan Krisha Shrestha",
-  address:"Bhakunde,Kavre",
-  phone:9812345678,
-  email:"shresthamohan828@gmail.com",
- }
- const education={
-  cousre:"B.Sc.CSIT",
-  collge:"Nist",
- }
+  const information = {
+    name: "Jan Kowalski",
+    address: "Birmingham,UK",
+    phone: 9812345678,
+    email: "someone123@gmail.com",
+  };
+  const education = {
+    course: "B.Sc.IT",
+    college: "University of manchester, 2015",
+  };
+  const skills = {
+    skills1: "Server management",
+    skills2: "Network Management",
+    skills3: "Security and Firewall Management",
+    skills4: "Troubleshooting",
+    skills5: "Backup and Digaster Recovery",
+    skills6: "Hardware Management",
+    skills7: "Directory Management",
+  };
 
-  return (
-    <div>
-      <Header name={information} education={education}/>
-    </div>
+  const technical={
     
 
-  )
+  }
+  return (
+    <div className="header_main">
+    <div>
+      <Header name={information} education={education} skills={skills} technical={technical} />
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,33 +1,64 @@
 /* eslint-disable react/prop-types */
 
 function Header(props) {
-    const {name, address, phone, email }=props.name;
-    const{}
+  const { name, address, phone, email } = props.name;
+  const { course, college } = props.education;
+  const { skills1, skills2, skills3, skills4, skills5, skills6, skills7 } =
+    props.skills;
   return (
     <>
-    <div>
+      <div>
         <h1>{name}</h1>
-    </div>
+      </div>
 
-    <div>
-    <h2>PERSONAL DETAIL</h2>
-    <div>
-        <b>Name:</b><br/>{name}<br/>
-        <b>Address:</b><br/>{address}<br/>
-        <b>Phone Number:</b><br/>{phone}<br/>
-        <b>Email:</b><br/>{email}<br/>
-    </div>
-    </div>
-    <div>
-        <h2>
-            EDUCATION
-        </h2>
+      <div>
+        <h4>PERSONAL DETAIL</h4>
         <div>
-
+          <b>Name:</b>
+          <br />
+          {name}
+          <br />
+          <b>Address:</b>
+          <br />
+          {address}
+          <br />
+          <b>Phone Number:</b>
+          <br />
+          {phone}
+          <br />
+          <b>Email:</b>
+          <br />
+          {email}
+          <br />
         </div>
-    </div>
+      </div>
+      <div>
+        <h4>EDUCATION</h4>
+        <div>
+          {course}
+          <br />
+          {college}
+        </div>
+      </div>
+      <div>
+        <h4>SKILLS</h4>
+        <div>
+          <ul>
+            <li> {skills1}</li>
+            <li> {skills2}</li>
+            <li> {skills3}</li>
+            <li> {skills4}</li>
+            <li> {skills5}</li>
+            <li> {skills6}</li>
+            <li> {skills7}</li>
+          </ul>
+        </div>
+        <div>
+            
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
